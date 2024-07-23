6f732e0fd0cbe7e334ff206be7d1d3981026aea4
jay-ekodus <jsardar@ekodusinc.com>
2024-07-23 13:07:07 +0530

    added server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import 'dotenv/config';
 
 // Initialize express app
 const app = express();
@@ -10,15 +11,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // Set the port
-const port = process.env.PORT || 5100;
+const port = process.env.VITE_NODE || 5100;
 
 // Start the server
 app.listen(port, () => {
